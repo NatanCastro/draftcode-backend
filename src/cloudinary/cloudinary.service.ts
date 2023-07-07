bc6bcfe3b5978fff969d6b9eadd3c5c16ce075a8
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-07-07 20:23:42 -0300

    configs

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -3,10 +3,17 @@ import { v2 } from 'cloudinary';
 
 @Injectable()
 export class CloudinaryService {
+  constructor() {
+    v2.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  }
+
   uploadToCloudinary = async (file: Express.Multer.File) => {
     return v2.uploader.upload(file.path, {
       folder: 'draft-project',
-      transformation: { quality: 'good' },
     });
   };
 
